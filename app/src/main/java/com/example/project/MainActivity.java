96ff85311dae445b5adc2f0fc117449d4413c941
bharadwaj0211 <bharadwaj.dasari@outlook.com>
2019-07-10 11:13:27 -0500

    Added Notification and action

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.project;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public  static String KEY = "name";
+    private NotificationCompat.Builder mNotifyBuilder;
+
+    private NotificationManager mNotifyManager;
+
+    private static final int NOTIFICATION_ID = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel();
     }
 
 
@@ -30,10 +40,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 break;
 
-            case R.id.signupbtn:
+            case R.id.notfctnbtn:
+                Intent dialIntent = new Intent(MainActivity.this,LoginActivity.class);
+                PendingIntent notifyPendingIntent =
+                        PendingIntent.getActivity(this,NOTIFICATION_ID,dialIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channelid")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Whatsapp")
+                        .setContentText("Update")
+                        .setProgress(100,60,false)
+                        .addAction(R.drawable.abc, "Reply", notifyPendingIntent )
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                mNotifyManager.notify(NOTIFICATION_ID, builder.build());
+                break;
 
         }
 
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "mychannel";
+            //getString(R.string.channel_name);
+            String description = "channel description";
+            //getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("channelid", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
